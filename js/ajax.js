7bf41ab1a7dd4e8e878017b5ad1499ebf5e86b8b
Mateusz Wójkowski <mat.wojkowski@gmail.com>
2017-08-27 11:19:04 +0200

    cwiczenie na zajeciach + warsztat

---
+++ js/ajax.js
@@ -0,0 +1,66 @@
+'use strict'
+
+/*$(function(){
+   $.ajax({ 
+       url:"http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+       dataType: 'json',
+       success: function(resultJSON) {
+           console.log(resultJSON);
+           
+           var pUserId = document.createElement('p');
+           var pUserName = document.createElement('p');
+           var pUserUrl = document.createElement('p');
+           
+           pUserId.innerHTML = 'User ID: ' + resultJSON.userId;
+           pUserName.innerHTML = 'User Name: ' + resultJSON.userName;
+           pUserUrl.innerHTML = 'User url http://: ' + resultJSON.userURL;
+           
+           document.body.appendChild(pUserId);
+           document.body.appendChild(pUserName);
+           document.body.appendChild(pUserUrl);
+       },
+       onerror: function (msg) {
+           console.log(msg);
+       }
+   }); */
+    
+    /*$.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function(data) {
+        console.log(data);
+    }); */
+});
+
+$ (a.button).click(function(){
+   console.log('button');
+});
+    
+$(function(){
+   $.ajax({
+      url: "http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl",
+       dataType: 'json',
+       success: function(resultJSON) {
+           console.log(resultJSON);
+           
+           var pUserId = document.createElement('p');
+           var pUserName = document.createElement('p');
+           var pUserUrl = document.createElement('p');
+           
+           pUserId.innerHTML = 'User ID: ' + resultJSON.userId;
+           pUserName.innerHTML = 'User Name: ' + resultJSON.userName;
+           pUserUrl.innerHTML = 'User url http://: ' + resultJSON.userURL;
+           
+           document.body.appendChild(pUserId);
+           document.body.appendChild(pUserName);
+           document.body.appendChild(pUserUrl);
+       },
+       onerror: function (msg) {
+           console.log(msg);
+       }
+       
+   }); 
+    
+    $.getJSON('http://echo.jsontest.com/userId/108/userName/Akademia108/userURL/akademia108.pl', function(data) {
+        console.log(data);
+        
+    });
+    
+});
